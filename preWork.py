6f0749c3567573588c2a013cc67f226a858bfbbf
Omar BD <dabar0@protonmail.com>
2024-03-06 09:33:59 -0600

    first commit

---
+++ preWork.py
@@ -0,0 +1,61 @@
+# # Question 1
+# # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
+# The first line of the code has been defined as below.
+
+def message(username):
+    print(f"hello_{username}",end="!")
+
+username = "USERNAME"
+message(username)
+
+# # Question 2
+# # Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+ 
+def first_odds():
+    a = [2,3,5,7]
+
+        
+# # Question 3
+# # Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+
+#     def max_num_in_list(a_list):
+#         .....
+
+# # Question 4
+# # Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not
+#  divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+#     def is_leap_year(a_year):
+#         .....     
+
+# # Question 5
+# # Write a function to check to see if all numbers in list are consecutive numbers. For example, 
+# [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean
+#  Type.
+
+#     def is_consecutive(a_list):
+#         
+    
+
+
+
+
+
+    # Cube Number Test... Print out all cubed numbers up to the total value 1000. 
+    # Meaning that if the cubed number is over 1000 break the loop.
+
+
+
+# cubed_number
+
+# def cubed_numbers():
+#     cubed_list = []
+#     for num in range(1, 1001):
+#         cubed = num ** 3
+#         if cubed > 1000:
+#             break
+#         cubed_list.append(cubed)
+#     print(cubed_list)
+
+# cubed_numbers()
